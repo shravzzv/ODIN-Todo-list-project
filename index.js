45cb790177598e54e5db90cb10655bb08743a6ba
sai shravan <saishravan384@gmail.com>
2023-09-15 13:55:49 +0530

    feat: Add List class with methods

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// declarations
 class Todo {
   constructor(title, desc, due, priority, notes, checklist) {
     this.title = title
@@ -7,31 +8,50 @@ class Todo {
     this.notes = notes
     this.checklist = checklist
     this.complete = false
+    this.list = 'inbox'
   }
 
   markComplete() {
     completeTodo(this)
   }
+
+  addToList(list) {
+    addTodoToList(this, list)
+  }
 }
 
 const completeTodo = (todo) => (todo.complete = true)
 
-const todo1 = new Todo(
-  'clear gmail/action needed',
-  '',
-  new Date(),
-  'high',
-  '',
-  ''
-)
-const todo2 = new Todo(
-  'clear gmail/pendig',
-  '',
-  new Date(2023, 8, 21),
-  '',
-  '',
-  ''
-)
-todo1.markComplete()
-console.log(todo1)
-console.log(todo2)
+const addTodoToList = (todo, list) => {
+  todo.list = list.title
+  list.todos.push(todo)
+}
+
+class List {
+  constructor(title) {
+    this.title = title
+    this.todos = []
+  }
+
+  removeTodo(todo) {
+    removeTodoFromList(todo, this)
+  }
+}
+
+const removeTodoFromList = (todo, list) => {
+  list.todos = list.todos.filter((item) => item.title !== todo.title)
+  todo.list = ''
+}
+
+// non declarations
+
+const todo1 = new Todo('clear gmail', '', new Date(), 'high', '', '')
+const todo2 = new Todo('take out trash', '', new Date('2023-09-15'), 3, '', '')
+
+const inbox = new List('inbox')
+const work = new List('work')
+
+todo1.addToList(inbox)
+todo2.addToList(work)
+
+work.removeTodo(todo2)
