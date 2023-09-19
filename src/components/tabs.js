63463180607c87dbc1f2ec3b53749f47e88116ee
sai shravan <saishravan384@gmail.com>
2023-09-19 13:37:22 +0530

    feat: Add tabs component

---
+++ src/components/tabs.js
@@ -0,0 +1,22 @@
+const lists = ['inbox', 'personal', 'work']
+
+const Tabs = () => {
+  const element = document.createElement('div')
+  element.className = 'tabs'
+
+  lists.forEach((list) => {
+    const tab = document.createElement('button')
+    tab.className = 'tab'
+    tab.textContent = list
+    element.appendChild(tab)
+  })
+
+  const addList = document.createElement('button')
+  addList.className = 'addBtn'
+  addList.textContent = '+'
+  element.appendChild(addList)
+
+  return element
+}
+
+export default Tabs
--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import './styles/main.css'
 import Todo from './todos/createTodo'
 import List from './lists/createList'
 import Logo from './components/logo'
+import Tabs from './components/tabs'
 
 document.addEventListener('DOMContentLoaded', () => {
   const content = document.querySelector('#content')
   content.appendChild(Logo())
+  content.appendChild(Tabs())
 })
