ef8f1f28df5a61efa3c2dc5fae0b191de102d445
sai shravan <saishravan384@gmail.com>
2023-09-20 14:34:55 +0530

    feat: Add new lists

--- src/index.js
+++ src/index.js
@@ -8,31 +8,52 @@ import AddTodo from './components/addTodo'
 import Todos from './components/todos'
 import NewListModal from './components/newListModal'
 
-const lists = ['inbox', 'personal', 'work', 'family', 'apartment']
+const lists = ['inbox', 'personal']
 
 document.addEventListener('DOMContentLoaded', () => {
   const content = document.querySelector('#content')
+
   content.appendChild(Logo())
-
   content.appendChild(Tabs(lists))
-
   content.appendChild(AddTodo())
-
   content.appendChild(Todos())
-
   content.appendChild(NewListModal())
 
+  const handleOpenNewListForm = (e) => {
+    document.querySelector('.newListForm').classList.toggle('show')
+    setTimeout(() => {
+      const input = document.querySelector('.newListForm>input')
+      if (input) {
+        input.focus()
+      }
+    }, 0)
+  }
+
+  const handleCloseNewListForm = (e) => {
+    document.querySelector('.newListForm').classList.toggle('show')
+    e.target.form.reset()
+  }
+
+  const handleListSubmit = (e) => {
+    e.preventDefault()
+    document.querySelector('.newListForm').classList.toggle('show')
+    const newList = new List(e.target.elements[0].value)
+    lists.push(newList.title)
+    e.target.reset()
+
+    const newTabs = Tabs(lists)
+    const tabsContainer = document.querySelector('.tabs')
+    tabsContainer.parentNode.replaceChild(newTabs, tabsContainer)
+    const addListBtn = document.querySelector('.addListBtn')
+    addListBtn.addEventListener('click', handleOpenNewListForm)
+  }
+
   const addListBtn = document.querySelector('.addListBtn')
+  addListBtn.addEventListener('click', handleOpenNewListForm)
 
-  const handleAddList = (e) => {
-    lists.push(new List('new').title)
-    // console.log(lists)
-    // content.appendChild(Tabs(lists))
-  }
+  const canceNewListBtn = document.querySelector('.newListForm .cancel')
+  canceNewListBtn.addEventListener('click', handleCloseNewListForm)
 
-  const handleCancelNewList = (e) => {
-    console.log(e)
-  }
-
-  addListBtn.addEventListener('click', handleAddList)
+  const newListForm = document.querySelector('.newListForm')
+  newListForm.addEventListener('submit', handleListSubmit)
 })
