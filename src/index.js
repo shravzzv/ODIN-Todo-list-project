3472612cae355c3fbca39486f690aae905eb2842
sai shravan <saishravan384@gmail.com>
2023-09-20 10:42:58 +0530

    feat: Add newListModal form

--- src/index.js
+++ src/index.js
@@ -6,11 +6,33 @@ import Logo from './components/logo'
 import Tabs from './components/tabs'
 import AddTodo from './components/addTodo'
 import Todos from './components/todos'
+import NewListModal from './components/newListModal'
+
+const lists = ['inbox', 'personal', 'work', 'family', 'apartment']
 
 document.addEventListener('DOMContentLoaded', () => {
   const content = document.querySelector('#content')
   content.appendChild(Logo())
-  content.appendChild(Tabs())
+
+  content.appendChild(Tabs(lists))
+
   content.appendChild(AddTodo())
+
   content.appendChild(Todos())
+
+  content.appendChild(NewListModal())
+
+  const addListBtn = document.querySelector('.addListBtn')
+
+  const handleAddList = (e) => {
+    lists.push(new List('new').title)
+    // console.log(lists)
+    // content.appendChild(Tabs(lists))
+  }
+
+  const handleCancelNewList = (e) => {
+    console.log(e)
+  }
+
+  addListBtn.addEventListener('click', handleAddList)
 })
